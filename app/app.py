90b23262058eb6c8de26a1ea65c9c11aa13002bf
Justin Zhang <dlyzxl@yahoo.com>
2025-08-18 18:07:29 -0500

    modify notebooks 00, 01

--- app/app.py
+++ app/app.py
@@ -1,3 +1,8 @@
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from utils.heart_score import calculate_heart_score
 from utils.translator import translate_text
